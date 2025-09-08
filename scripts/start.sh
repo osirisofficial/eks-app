0a137c056da18c0c5ead1854c9d9fe9a74f32d60
osirisofficial <kondamanas.official@gmail.com>
2025-09-08 17:11:58 +0530

    issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ set -e
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 783764591716.dkr.ecr.us-east-1.amazonaws.com
 
 #run container
-dcoker run -d -p 8080:80 783764591716.dkr.ecr.us-east-1.amazonaws.com/eks-app:latest  --name=netflix
+docker run -d -p 8080:80 783764591716.dkr.ecr.us-east-1.amazonaws.com/eks-app:latest  --name=netflix

5c02d412cdf6e8873e93f7d2525892774d89da7e
osirisofficial <kondamanas.official@gmail.com>
2025-09-08 17:32:58 +0530

    changes

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,16 @@
-docker stop netflix
-docker rm netflix
+#!/bin/sh
+
+# Get IDs of all running containers
+RUNNING_CONTAINERS=$(docker ps -q)
+
+if [ -n "$RUNNING_CONTAINERS" ]; then
+  echo "Stopping all running containers..."
+  docker stop $RUNNING_CONTAINERS
+
+  echo "Removing containers..."
+  docker rm $RUNNING_CONTAINERS
+else
+  echo "No running containers found."
+fi
+
 docker image rm 783764591716.dkr.ecr.us-east-1.amazonaws.com/eks-app:latest

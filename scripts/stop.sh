361b8dcaea70268a3ca7b4ccc4952cdd5d30841d
osirisofficial <kondamanas.official@gmail.com>
2025-09-08 17:43:33 +0530

    fix issue of removing container

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# Get IDs of all running containers
-RUNNING_CONTAINERS=$(docker ps -q)
-
-if [ -n "$RUNNING_CONTAINERS" ]; then
-  echo "Stopping all running containers..."
-  docker stop $RUNNING_CONTAINERS
-
-  echo "Removing containers..."
-  docker rm $RUNNING_CONTAINERS
-else
-  echo "No running containers found."
-fi
+docker rm -f $(docker ps -aq)
 
 docker image rm 783764591716.dkr.ecr.us-east-1.amazonaws.com/eks-app:latest
